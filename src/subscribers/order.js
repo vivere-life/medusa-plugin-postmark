a153af773477b386a31b548f644194f3d2499139
Oleksandra Nedashkivska <oleksandra.nedashkivska@binary-studio.com>
2024-11-13 11:04:41 +0200

    fix: update subscription

--- src/subscribers/order.js
+++ src/subscribers/order.js
@@ -14,7 +14,6 @@ class OrderSubscriber {
     this.notificationService_.subscribe("order.shipment_created", "postmark");
     this.notificationService_.subscribe("customer.created", "postmark");
     this.notificationService_.subscribe("customer.password_reset", "postmark");
-    this.notificationService_.subscribe("custom.contact_us", "postmark");
   }
 }
 
--- src/subscribers/user.js
+++ src/subscribers/user.js
@@ -1,53 +1,60 @@
 class UserSubscriber {
-   constructor({ eventBusService, postmarkService }) {
-     this.postmarkService_ = postmarkService
-     this.eventBus_ = eventBusService
+  constructor({ eventBusService, postmarkService }) {
+    this.postmarkService_ = postmarkService;
+    this.eventBus_ = eventBusService;
 
-       this.eventBus_.subscribe("user.password_reset", async (data) => {
-           await this.postmarkService_.sendNotification(
-               "user.password_reset",
-               data,
-               null
-           )
-       })
-       this.eventBus_.subscribe("user.created", async (data) => {
-           await this.postmarkService_.sendNotification(
-               "user.created",
-               data,
-               null
-           )
-       })
+    this.eventBus_.subscribe("user.password_reset", async (data) => {
+      await this.postmarkService_.sendNotification(
+        "user.password_reset",
+        data,
+        null
+      );
+    });
 
-       this.eventBus_.subscribe("auth.password_reset", async (data) => {
-           await this.postmarkService_.sendNotification(
-               "auth.password_reset",
-               data,
-               null
-           )
-       })
-       this.eventBus_.subscribe("auth.verify_account", async (data) => {
-           await this.postmarkService_.sendNotification(
-               "auth.verify_account",
-               data,
-               null
-           )
-       })
+    this.eventBus_.subscribe("user.created", async (data) => {
+      await this.postmarkService_.sendNotification("user.created", data, null);
+    });
 
-       this.eventBus_.subscribe("activity.inactive_user", async (data) => {
-           await this.postmarkService_.sendNotification(
-               "activity.inactive_user",
-               data,
-               null
-           )
-       })
-       this.eventBus_.subscribe("activity.inactive_customer", async (data) => {
-           await this.postmarkService_.sendNotification(
-               "activity.inactive_customer",
-               data,
-               null
-           )
-       })
-   }
- }
+    this.eventBus_.subscribe("auth.password_reset", async (data) => {
+      await this.postmarkService_.sendNotification(
+        "auth.password_reset",
+        data,
+        null
+      );
+    });
 
- export default UserSubscriber
+    this.eventBus_.subscribe("auth.verify_account", async (data) => {
+      await this.postmarkService_.sendNotification(
+        "auth.verify_account",
+        data,
+        null
+      );
+    });
+
+    this.eventBus_.subscribe("activity.inactive_user", async (data) => {
+      await this.postmarkService_.sendNotification(
+        "activity.inactive_user",
+        data,
+        null
+      );
+    });
+
+    this.eventBus_.subscribe("activity.inactive_customer", async (data) => {
+      await this.postmarkService_.sendNotification(
+        "activity.inactive_customer",
+        data,
+        null
+      );
+    });
+
+    this.eventBus_.subscribe("custom.contact_us", async (data) => {
+      await this.postmarkService_.sendNotification(
+        "custom.contact_us",
+        data,
+        null
+      );
+    });
+  }
+}
+
+export default UserSubscriber;

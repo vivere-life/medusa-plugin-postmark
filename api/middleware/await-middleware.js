c16f1e13c3c4932e77b171249c603ca3392a9f0c
nikita-remeslov <nikita.remeslov@binary-studio.com>
2024-11-14 15:20:25 +0100

    chore: fix build

--- api/middleware/await-middleware.js
+++ api/middleware/await-middleware.js
@@ -1,13 +1,11 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true,
+  value: true
 });
 exports["default"] = void 0;
-var _default = (exports["default"] = function _default(fn) {
+var _default = exports["default"] = function _default(fn) {
   return function () {
-    return fn
-      .apply(void 0, arguments)
-      ["catch"](arguments.length <= 2 ? undefined : arguments[2]);
+    return fn.apply(void 0, arguments)["catch"](arguments.length <= 2 ? undefined : arguments[2]);
   };
-});
+};
--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,13 +1,11 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true,
+  value: true
 });
 exports["default"] = void 0;
 var _awaitMiddleware = _interopRequireDefault(require("./await-middleware"));
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
-var _default = (exports["default"] = {
-  wrap: _awaitMiddleware["default"],
-});
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var _default = exports["default"] = {
+  wrap: _awaitMiddleware["default"]
+};

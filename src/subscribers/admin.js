2aca3ac6879d06b8c12944ae5331ae5429aa3249
liamukm <liam@ukmeds.co.uk>
2024-12-16 15:20:03 +0000

    Chore: added invite.created to postmark templates

---
+++ src/subscribers/admin.js
@@ -0,0 +1,14 @@
+class AdminSubscriber {
+  constructor({ eventBusService, postmarkService }) {
+    this.postmarkService_ = postmarkService;
+    this.eventBus_ = eventBusService;
+
+    this.eventBus_.subscribe('invite.created', async (data) => {
+      await this.postmarkService_.sendNotification(
+        'invite.created',
+        data,
+        null
+      );
+    });
+  }
+}
